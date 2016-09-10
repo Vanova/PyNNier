b05938eae131942e0c8a7a8383305cbbe996fa0e
Vanova <iva427@mail.ru>
2016-09-10 17:05:05 +0300

    started Jacobian experiment: what happen if Jacobian is diagonal dominant and drop off-diagonal

---
+++ experiment/jacobian_approx.py
@@ -0,0 +1,16 @@
+"""
+Experiment: check whether the dropping of off-diagonal elements
+    of Jacobian will degrade the gradient descend convergence.
+    Q: What conditions need to be formulated for eigenvalues of Jacobian?
+"""
+
+def multivar_vector_function():
+    pass
+
+
+def gradience_check():
+    """
+    See computer vision course by Karpathy
+    :return:
+    """
+    pass
