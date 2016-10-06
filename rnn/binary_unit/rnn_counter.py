6c21fc82c9f4bbac3fbf09b51a2ebb64f45d58d8
Vanova <iva427@mail.ru>
2016-10-06 03:22:12 +0300

    refactor plotter for the RNN counter of the error surface, fixed interfaces

--- rnn/binary_unit/rnn_counter.py
+++ rnn/binary_unit/rnn_counter.py
@@ -22,60 +22,54 @@ class RNNBinaryCounter():
         self.W = [-1.5, 2.]  # wx, wRec
 
     # Define the forward step functions
-    def update_state(self, xk, sk, wx, wRec):
+    def _update_state(self, xk, sk):
         """
         Compute state k from the previous state (sk) and current input (xk),
         by use of the input weights (wx) and recursive weights (wRec).
         """
-        return xk * wx + sk * wRec
+        return xk * self.W[0] + sk * self.W[1]
 
-    def forward_states(self, X, wx, wRec):
-        """
-        Unfold the network and compute all state activations given the input X,
-        and input weights (wx) and recursive weights (wRec).
-        Return the state activations in a matrix, the last column S[:,-1] contains the
-        final activations.
-        """
+    def feedforward(self, x):
         # Initialise the matrix that holds all states for all input sequences.
         # The initial state s0 is set to 0.
-        S = np.zeros((X.shape[0], X.shape[1] + 1))
-        for k in range(0, X.shape[1]):
-            S[:, k + 1] = self.update_state(X[:, k], S[:, k], wx, wRec)
+        S = np.zeros((x.shape[0], x.shape[1] + 1))
+        for k in range(0, x.shape[1]):
+            S[:, k + 1] = self._update_state(x[:, k], S[:, k])
         return S
 
-    def backward_gradient(self, X, S, grad_out, wRec):
+    def backward_gradient(self, x, t, s):
         """
         Backpropagate the gradient computed at the output (grad_out) through the network.
         Accumulate the parameter gradients for wX and wRec by for each layer by addition.
         Return the parameter gradients as a tuple, and the gradients at the output of each layer.
         """
         # Initialise the array that stores the gradients of the cost with respect to the states.
-        grad_over_time = np.zeros((X.shape[0], X.shape[1] + 1))
-        grad_over_time[:, -1] = grad_out
+        grad_over_time = np.zeros((x.shape[0], x.shape[1] + 1))
+        grad_over_time[:, -1] = self._delta(s[:, -1], t)
         # Set the gradient accumulations to 0
         wx_grad = 0
         wRec_grad = 0
-        for k in range(X.shape[1], 0, -1):
+        for k in range(x.shape[1], 0, -1):
             # Compute the parameter gradients and accumulate the results.
-            wx_grad += np.sum(grad_over_time[:, k] * X[:, k - 1])
-            wRec_grad += np.sum(grad_over_time[:, k] * S[:, k - 1])
+            wx_grad += np.sum(grad_over_time[:, k] * x[:, k - 1])
+            wRec_grad += np.sum(grad_over_time[:, k] * s[:, k - 1])
             # Compute the gradient at the output of the previous layer
-            grad_over_time[:, k - 1] = grad_over_time[:, k] * wRec
+            grad_over_time[:, k - 1] = grad_over_time[:, k] * self.W[1]
         return (wx_grad, wRec_grad), grad_over_time
 
-    def output_gradient(self, y, t):
+    def _delta(self, y, t):
         """
         Compute the gradient of the MSE cost function with respect to the output y.
         """
         n_samples = t.shape[0]
         return 2.0 * (y - t) / n_samples
 
-    def get_grad_over_time(self, wx, wRec, data, lab):
-        """Helper func to only get the gradient over time from wx and wRec."""
-        S = self.forward_states(data, wx, wRec)
-        grad_out = self.output_gradient(S[:, -1], lab).sum()
-        _, grad_over_time = self.backward_gradient(data, S, grad_out, wRec)
-        return grad_over_time
+    # def get_grad_over_time(self, wx, wRec, data, lab):
+    #     """Helper func to only get the gradient over time from wx and wRec."""
+    #     S = self.forward_states(data, wx, wRec)
+    #     grad_out = self.delta(S[:, -1], lab).sum()
+    #     _, grad_over_time = self.backward_gradient(data, S, grad_out, wRec)
+    #     return grad_over_time
 
     def cost_value(self, y, t):
         """
@@ -91,18 +85,16 @@ class RNNBinaryCounter():
 
         for i in range(500):
             # Get the update values and sign of the last gradient
-            W_delta, W_sign = network.step_rprop(data, labels, W_sign, W_delta, eta_p, eta_n)
+            W_delta, W_sign = network._step_rprop(data, labels, W_sign, W_delta, eta_p, eta_n)
             # Update each weight parameter separately
             for i, _ in enumerate(self.W):
                 self.W[i] -= W_sign[i] * W_delta[i]
             list_ws.append((self.W[0], self.W[1]))  # Add weights to list to plot
-
         print('Final weights are: wx = {0},  wRec = {1}'.format(self.W[0], self.W[1]))
-
         return list_ws
 
     # Define Rprop optimisation function
-    def step_rprop(self, x, t, W_prev_sign, W_delta, eta_p, eta_n):
+    def _step_rprop(self, x, t, W_prev_sign, W_delta, eta_p, eta_n):
         """
         Update Rprop values in one iteration.
         X: input data.
@@ -112,11 +104,10 @@ class RNNBinaryCounter():
         eta_p, eta_n: Rprop hyperparameters.
         """
         # Perform forward and backward pass to get the gradients
-        S = self.forward_states(x, self.W[0], self.W[1])
-        grad_out = self.output_gradient(S[:, -1], t)
-        W_grads, _ = self.backward_gradient(x, S, grad_out, self.W[1])
+        states = self.feedforward(x)
+        W_grads, _ = self.backward_gradient(x, t, states)
         W_sign = np.sign(W_grads)  # Sign of new gradient
-        # Update the Delta (update value) for each weight parameter seperately
+        # Update the Delta (update value) for each weight parameter separately
         for i, _ in enumerate(self.W):
             if W_sign[i] == W_prev_sign[i]:
                 W_delta[i] *= eta_p
@@ -135,19 +126,21 @@ def gradient_check(X, Y, net):
     # Set the small change to compute the numerical gradient
     eps = 1e-7
     # Compute the backprop gradients
-    S = net.forward_states(X, params[0], params[1])
-    grad_out = net.output_gradient(S[:, -1], Y)
-    backprop_grads, grad_over_time = net.backward_gradient(X, S, grad_out, params[1])
+    net.W = params
+    S = net.feedforward(X)
+    backprop_grads, grad_over_time = net.backward_gradient(X, Y, S)
 
     # Compute the numerical gradient for each parameter in the layer
     for p_idx, _ in enumerate(params):
         grad_backprop = backprop_grads[p_idx]
         # + eps
         params[p_idx] += eps
-        plus_cost = net.cost_value(net.forward_states(X, params[0], params[1])[:, -1], Y)
+        net.W = params
+        plus_cost = net.cost_value(net.feedforward(X)[:, -1], Y)
         # - eps
         params[p_idx] -= 2 * eps
-        minus_cost = net.cost_value(net.forward_states(X, params[0], params[1])[:, -1], Y)
+        net.W = params
+        minus_cost = net.cost_value(net.feedforward(X)[:, -1], Y)
         # reset param value
         params[p_idx] += eps
         # calculate numerical gradient
@@ -158,7 +151,6 @@ def gradient_check(X, Y, net):
                 'Numerical gradient of {:.6f} is not close to the backpropagation gradient of {:.6f}!'.format(
                     float(grad_num), float(grad_backprop)))
     print('No gradient errors found')
-
 
 
 # create dataset
@@ -174,12 +166,17 @@ data, labels = dataset(num_samples=nb_of_samples, seq_len=sequence_len)
 # # Show figures
 # plt.show()
 
+# TODO:
+# network = RNNBinaryCounter()
+# NetTrainer(network, method=NetTrainer.rprop, visualize=True)
+
 network = RNNBinaryCounter()
-gradient_check(data,labels, network)
+gradient_check(data, labels, network)
+network = RNNBinaryCounter()
 # Rprop training
 eta_p = 1.2
 eta_n = 0.5
 list_of_ws = network.rprop_training(eta_p, eta_n)
 # plot the cost surface with the weights over the iterations
 net_viz = NetworkVisualiser()
-net_viz.plot_optimisation(list_of_ws, lambda w1, w2: network.cost_value(network.forward_states(data, w1, w2)[:, -1], labels))
+net_viz.plot_network_optimisation(network, list_of_ws, data, labels)
--- utils/plotters.py
+++ utils/plotters.py
@@ -19,7 +19,7 @@ class NetworkVisualiser():
     def __init__(self):
         pass
 
-    def get_cost_surface(self, w1_low, w1_high, w2_low, w2_high, nb_of_ws, cost_func):
+    def get_cost_surface(self, w1_low, w1_high, w2_low, w2_high, nb_of_ws, network, data, labels):
         """Define a vector of weights for which we want to plot the cost."""
         w1 = np.linspace(w1_low, w1_high, num=nb_of_ws)  # Weight 1
         w2 = np.linspace(w2_low, w2_high, num=nb_of_ws)  # Weight 2
@@ -28,32 +28,22 @@ class NetworkVisualiser():
         # Fill the cost matrix for each combination of weights
         for i in range(nb_of_ws):
             for j in range(nb_of_ws):
-                cost_ws[i, j] = cost_func(ws1[i, j], ws2[i, j])
+                network.W = [ws1[i, j], ws2[i, j]]
+                cost_ws[i, j] = network.cost_value(network.feedforward(data)[:, -1], labels)
         return ws1, ws2, cost_ws
 
     def plot_error_surface(self, network):
         pass
 
-    def plot_network_optimisation(self, network, data, labels):
-
-        pass
-
-    def plot_surface(self, ax, ws1, ws2, cost_ws):
-        """Plot the cost in function of the weights."""
-        surf = ax.contourf(ws1, ws2, cost_ws, levels=np.logspace(-0.2, 8, 30), cmap=cm.pink, norm=LogNorm())
-        ax.set_xlabel('$w_{in}$', fontsize=15)
-        ax.set_ylabel('$w_{rec}$', fontsize=15)
-        return surf
-
-    def plot_optimisation(self, ls_of_ws, cost_func):
+    def plot_network_optimisation(self, network, list_of_ws, data, labels):
         """Plot the optimisation iterations on the cost surface."""
-        ws1, ws2 = zip(*ls_of_ws)
+        ws1, ws2 = zip(*list_of_ws)
         # Plot figures
-        fig = plt.figure(figsize=(10, 4))
+        fig = plt.figure(figsize=(5, 4))
 
         # Plot overview of cost function
-        ax_1 = fig.add_subplot(1, 2, 1)
-        ws1_1, ws2_1, cost_ws_1 = self.get_cost_surface(-3, 3, -3, 3, 100, cost_func)
+        ax_1 = fig.add_subplot(1, 1, 1)
+        ws1_1, ws2_1, cost_ws_1 = self.get_cost_surface(-3, 3, -3, 3, 100, network, data, labels)
         surf_1 = self.plot_surface(ax_1, ws1_1, ws2_1, cost_ws_1 + 1)
         ax_1.plot(ws1, ws2, 'b.')
         ax_1.set_xlim([-3, 3])
@@ -67,3 +57,10 @@ class NetworkVisualiser():
         cbar.set_ticklabels(['{:.0e}'.format(i) for i in np.logspace(0, 8, 9)])
         plt.suptitle('Cost surface', fontsize=15)
         plt.show()
+
+    def plot_surface(self, ax, ws1, ws2, cost_ws):
+        """Plot the cost in function of the weights."""
+        surf = ax.contourf(ws1, ws2, cost_ws, levels=np.logspace(-0.2, 8, 30), cmap=cm.pink, norm=LogNorm())
+        ax.set_xlabel('$w_{in}$', fontsize=15)
+        ax.set_ylabel('$w_{rec}$', fontsize=15)
+        return surf
