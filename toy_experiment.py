09e76302b96d637a339fc2490d49a944ce337eb5
Vanova <iva427@mail.ru>
2016-10-17 04:01:01 +0300

    implemented network decision surface and scatter plot

--- toy_experiment.py
+++ toy_experiment.py
@@ -1,70 +1,17 @@
 import numpy as np
+import copy
 import matplotlib.pyplot as plt
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
 from utils import toy_loader
 from sklearn import preprocessing
-from utils.plotters import NetworkVisualiser
+import utils.plotters as viz
 from ann import network
 from ann import mif_network
 
-COLORS = np.array(['!',
-                   '#FF3333',  # red [1, 0]
-                   '#0198E1',  # blue [0, 1]
-                   '#4DBD33',  # green [1, 1]
-                   '#FCD116',  # yellow
-                   '#FF7216',  # orange
-                   '#BF5FFF',  # purple
-                   '#87421F'  # brown
-                   ])
-MARKERS = np.array(['!',
-                    '^',  # [1, 0]
-                    'v',  # [0, 1]
-                    'o'  # [1, 1]
-                    ])
 SET_TITLES = ['Training set',
               'Test set',
               'Dev set']
-
-
-def plot_hyperplane(ax, clf, min_x, max_x, linestyle, label):
-    # get the separating hyperplane
-    w = clf.coef_[0]
-    a = -w[0] / w[1]
-    xx = np.linspace(min_x - 5, max_x + 5)  # make sure the line is long enough
-    yy = a * xx - (clf.intercept_[0]) / w[1]
-    ax.plot(xx, yy, linestyle, label=label)
-
-
-def plot_2d(figure_axes, x, y):
-    # plot dots
-    id_lbl = (y * [1, 2]).sum(axis=1)
-    colors = COLORS.take(id_lbl)
-    for c, m in zip(COLORS, MARKERS):
-        xr = x[np.where(colors == c)]  # take class by color
-        if len(xr):
-            figure_axes.scatter(xr[:, 0], xr[:, 1], color=c, marker=m, edgecolor='black')
-
-    # prepare hyperplanes
-    # min_x = np.min(x[:, 0])
-    # max_x = np.max(x[:, 0])
-    #
-    # classif = OneVsRestClassifier(SVC(kernel='linear'))
-    # classif.fit(x, y)
-    # plot_hyperplane(figure_axes, classif.estimators_[0], min_x, max_x, 'k--',
-    #                 'Boundary\nfor class 1')
-    # plot_hyperplane(figure_axes, classif.estimators_[1], min_x, max_x, 'k-.',
-    #                 'Boundary\nfor class 2')
-
-
-def plot_3d(figure_axes, x, y):
-    id_lbl = (y * [1, 2]).sum(axis=1)
-    colors = COLORS.take(id_lbl)
-    for c, m in zip(COLORS, MARKERS):
-        xr = x[np.where(colors == c)]
-        if len(xr):
-            figure_axes.scatter(xr[:, 0], xr[:, 1], xr[:, 2], color=c, marker=m, edgecolor='b')
-
 
 def plot_toy_data(data_list, titles, feature_dim):
     # TODO feat dim 2D or 3D
@@ -76,7 +23,7 @@ def plot_toy_data(data_list, titles, feature_dim):
         for i, ax in enumerate(fig.axes):
             ax.set_title(titles[i])
             tr_x, tr_y = toy_loader.plot_format(data_list[i])
-            plot_2d(ax, tr_x, tr_y)
+            viz.plot_2d_scatter(ax, tr_x, tr_y)
         plt.show()
     if feature_dim == 3:
         fig = plt.figure(figsize=(12, 5))
@@ -84,7 +31,7 @@ def plot_toy_data(data_list, titles, feature_dim):
             ax = fig.add_subplot(1, len(data_list), i + 1, projection='3d')
             ax.set_title(titles[i])
             tr_x, tr_y = toy_loader.plot_format(data_list[i])
-            plot_3d(ax, tr_x, tr_y)
+            viz.plot_3d_scatter(ax, tr_x, tr_y)
         plt.show()
     if feature_dim > 3:
         print("Not implemented...")
@@ -104,23 +51,23 @@ def data_stats(data):
 # TODO:
 # + implement NN saving
 # 2. plot model weights
-# 2.1. plot data before and after classification
+# + plot data before and after classification
 # 2.2. Plot value of d_k based on the outputs of sigma
 # + plot toy 2D data: plot_toy
 # + plot toy 3D data: plot_toy
 # + plot loss surface
 # + simple architecture [2, 2] or [3, 3]
-# 5. make video of parameters optimisation
 
-# Plot data scatter distribution
+#####
+# Prepare data and plot data scatter distribution
+#####
 feature_dim = 2
-# train_data, dev_data, test_data = toy_loader.load_data(n_tr=250, n_dev=50, n_tst=50,
-#                                                        n_features=feature_dim, n_classes=2)
 train_data, dev_data, test_data = toy_loader.load_data(n_tr=250, n_dev=50, n_tst=50,
                                                        n_features=feature_dim, n_classes=2,
                                                        scaler=preprocessing.StandardScaler())
-plot_toy_data([train_data, dev_data, test_data], SET_TITLES, feature_dim)
-data_stats(train_data)
+# plot_toy_data([train_data, dev_data, test_data], SET_TITLES, feature_dim)
+# data_stats(train_data)
+
 #####
 # Simple toy experiment
 # 1.MSE loss and Sigmoid outputs
@@ -132,19 +79,19 @@ architecture = [feature_dim, 2]
 network = network.Network(architecture)
 eval, loss, list_ws = network.SGD(train_data, epochs, mini_batch, learn_rate,
                                   test_data=test_data, is_list_weights=True)
-best_weights = network.weights
+best_weights = copy.deepcopy(network.weights)
 print("mF1 on test set: {0}".format(eval[-1]))
 print("MSE loss: {0}".format(loss[-1]))
 print("Network optimal weights:")
-print(best_weights)
+print(best_weights[-1])
 #####
 # visualize the network weights
 #####
-net_viz = NetworkVisualiser()
+net_viz = viz.NetworkVisualiser()
 net_viz.plot_neurons_cost_surface(network, train_data, title="The MSE error surface")
 # visualize decision boundary
 network.weights = best_weights
-net_viz.plot_decision_boundaries(network, train_data, title="The MSE error surface")
+net_viz.plot_decision_boundaries(network, train_data, title="Decision surface")
 
 
 # file_net = "./data/experiment/toy/toy_epo_{0}_btch_{1}_lr_{2}". \
--- utils/plotters.py
+++ utils/plotters.py
@@ -6,6 +6,61 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import colorConverter, ListedColormap
 import toy_loader
 
+COLORS = np.array(['!',
+                   '#FF3333',  # red [1, 0]
+                   '#0198E1',  # blue [0, 1]
+                   '#4DBD33',  # green [1, 1]
+                   '#FCD116',  # yellow
+                   '#FF7216',  # orange
+                   '#BF5FFF',  # purple
+                   '#87421F'  # brown
+                   ])
+MARKERS = np.array(['!',
+                    '^',  # [1, 0]
+                    'v',  # [0, 1]
+                    'o'  # [1, 1]
+                    ])
+
+
+def plot_2d_scatter(figure_axes, x, y):
+    # TODO number of classes
+    # plot dots
+    id_lbl = (y * [1, 2]).sum(axis=1)
+    colors = COLORS.take(id_lbl)
+    for c, m in zip(COLORS, MARKERS):
+        xr = x[np.where(colors == c)]  # take class by color
+        if len(xr):
+            figure_axes.scatter(xr[:, 0], xr[:, 1], color=c, marker=m, edgecolor='black')
+
+            # prepare hyperplanes
+            # min_x = np.min(x[:, 0])
+            # max_x = np.max(x[:, 0])
+            #
+            # classif = OneVsRestClassifier(SVC(kernel='linear'))
+            # classif.fit(x, y)
+            # plot_hyperplane(figure_axes, classif.estimators_[0], min_x, max_x, 'k--',
+            #                 'Boundary\nfor class 1')
+            # plot_hyperplane(figure_axes, classif.estimators_[1], min_x, max_x, 'k-.',
+            #                 'Boundary\nfor class 2')
+
+
+def plot_hyperplane(ax, clf, min_x, max_x, linestyle, label):
+    # get the separating hyperplane
+    w = clf.coef_[0]
+    a = -w[0] / w[1]
+    xx = np.linspace(min_x - 5, max_x + 5)  # make sure the line is long enough
+    yy = a * xx - (clf.intercept_[0]) / w[1]
+    ax.plot(xx, yy, linestyle, label=label)
+
+
+def plot_3d_scatter(figure_axes, x, y):
+    id_lbl = (y * [1, 2]).sum(axis=1)
+    colors = COLORS.take(id_lbl)
+    for c, m in zip(COLORS, MARKERS):
+        xr = x[np.where(colors == c)]
+        if len(xr):
+            figure_axes.scatter(xr[:, 0], xr[:, 1], xr[:, 2], color=c, marker=m, edgecolor='b')
+
 
 def show_curve(y, labels, title):
     """
@@ -30,17 +85,18 @@ class NetworkVisualiser():
 
     def plot_neurons_cost_surface(self, network, data, fun_name="$E$", title="Cost function surface"):
         # Data vectorisation
-        x, y = map(list, zip(*data))
-        xvec = np.array([s.flatten() for s in x]).T
-        yvec = np.array([s.flatten() for s in y]).T
-        # visualize the weights of the last layer
+        # TODO refactor
+        smps, labs = map(list, zip(*data))
+        xvec = np.array([s.flatten() for s in smps]).T
+        yvec = np.array([s.flatten() for s in labs]).T
+        # Visualize the weights of the last layer
         fig = plt.figure(figsize=plt.figaspect(0.5))
         id_layer = len(network.weights) - 1
         neurons = network.weights[id_layer].shape[0]
         for i in xrange(neurons):
-            ax = fig.add_subplot(2, neurons, i + 1, projection='3d')
             ws1, ws2, cost_ws = self._cost_surface_grid(-3, 3, -3, 3, 100, network, xvec, yvec, id_layer, i)
             labs = ["$w_{%d1}$" % (i + 1), "$w_{%d2}$" % (i + 1), fun_name]
+            ax = fig.add_subplot(2, neurons, i + 1, projection='3d')
             self._surface_view(ax, ws1, ws2, cost_ws, labs)
             # surface projection
             ax = fig.add_subplot(2, neurons, (i + 1) + neurons)
@@ -50,40 +106,23 @@ class NetworkVisualiser():
         plt.show()
         # TODO 3D weights case
 
-    def plot_decision_boundaries(self, network, data, fun_name="$E$", title="Network decision boundary"):
+    def plot_decision_boundaries(self, network, data, title="Network decision boundary"):
         """
         Plot decision boundary of every neuron
         """
-        id_neuron = 0
-        nb_of_xs = 100
-        xs1 = np.linspace(-3, 3, num=nb_of_xs)
-        xs2 = np.linspace(-3, 3, num=nb_of_xs)
-        xx, yy = np.meshgrid(xs1, xs2)  # create the grid
+        fig = plt.figure(figsize=plt.figaspect(0.5))
         # Initialize and fill the classification plane
-        classification_plane = np.zeros((nb_of_xs, nb_of_xs))
-        # TODO vectorise (xx,yy)
-        for i in range(nb_of_xs):
-            for j in range(nb_of_xs):
-                pred = network.feedforward(np.asarray([[xx[i, j]], [yy[i, j]]]))
-                # TODO plot for one neuron
-                classification_plane[i, j] = pred[id_neuron, 0]
-        # Create a color map to show the classification colors of each grid point
-        # cmap = ListedColormap([
-        #     colorConverter.to_rgba('b', alpha=0.30),
-        #     colorConverter.to_rgba('r', alpha=0.30)])
-
-        # Plot the classification plane with decision boundary and input samples
-        plt.contourf(xx, yy, classification_plane, cmap=cm.coolwarm)
-        # Plot both classes on the x1, x2 plane
-        # TODO plot data scatter
-        # plt.plot(x_red[:, 0], x_red[:, 1], 'ro', label='class red')
-        # plt.plot(x_blue[:, 0], x_blue[:, 1], 'bo', label='class blue')
-        # plt.grid()
-        # plt.legend(loc=1)
-        # plt.xlabel('$x_1$', fontsize=15)
-        # plt.ylabel('$x_2$', fontsize=15)
-        # plt.axis([-1.5, 1.5, -1.5, 1.5])
-        plt.title(title)
+        neurons = network.weights[-1].shape[0]
+        for n in xrange(neurons):
+            xx, yy, class_score = self._decision_grid(network, n)
+            # Decision plane
+            ax = fig.add_subplot(1, neurons, n + 1)
+            cntr = self._contour_view(ax, xx, yy, class_score, ["$x$", "$y$"], grid=False)
+            # Data scatter plot
+            data_x, data_y = toy_loader.plot_format(data)
+            plot_2d_scatter(ax, data_x, data_y)
+            fig.colorbar(cntr, ticks=np.linspace(0, 1, 9))
+        plt.suptitle(title, fontsize=15)
         plt.show()
 
     def plot_network_optimisation(self, network, list_of_ws, data, labels):
@@ -130,7 +169,20 @@ class NetworkVisualiser():
         leg = plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), frameon=False, numpoints=1)
         leg.set_title('$(w_x, w_{rec})$', prop={'size': 15})
 
-    def _cost_surface_grid(self, w1_low, w1_high, w2_low, w2_high, nb_of_ws, network, data, labels, layer, neuron):
+    def _decision_grid(self, network, neuron_id):
+        nb_of_xs = 100
+        xs1 = np.linspace(-3, 3, num=nb_of_xs)
+        xs2 = np.linspace(-3, 3, num=nb_of_xs)
+        xx, yy = np.meshgrid(xs1, xs2)
+        # Initialize and fill the classification plane
+        class_surface = np.zeros((nb_of_xs, nb_of_xs))
+        for i in range(nb_of_xs):
+            xy = np.array([xx[i], yy[i]])
+            pred = network.feedforward(xy)
+            class_surface[i] = pred[neuron_id]
+        return xx, yy, class_surface
+
+    def _cost_surface_grid(self, w1_low, w1_high, w2_low, w2_high, nb_of_ws, network, data, labels, layer, neuron_id):
         """Define a vector of weights for which we want to plot the cost."""
         w1 = np.linspace(w1_low, w1_high, num=nb_of_ws)  # Weight 1
         w2 = np.linspace(w2_low, w2_high, num=nb_of_ws)  # Weight 2
@@ -139,7 +191,7 @@ class NetworkVisualiser():
         # Fill the cost matrix for each combination of weights
         for i in range(nb_of_ws):
             for j in range(nb_of_ws):
-                network.weights[layer][neuron] = np.array([ws1[i, j], ws2[i, j]])
+                network.weights[layer][neuron_id] = np.array([ws1[i, j], ws2[i, j]])
                 cost_ws[i, j] = network.cost_value(network.feedforward(data), labels)
         return ws1, ws2, cost_ws
 
@@ -168,9 +220,11 @@ class NetworkVisualiser():
         ax.set_ylim([-3, 3])
         return surf
 
-    def _contour_view(self, ax, ws1, ws2, cost_ws, labels):
-        surf = ax.contourf(ws1, ws2, cost_ws, cmap=cm.coolwarm)
+    def _contour_view(self, ax, ws1, ws2, cost_ws, labels, grid=True):
+        cntr = ax.contourf(ws1, ws2, cost_ws, cmap=cm.coolwarm, alpha=0.5)
         ax.set_xlabel(labels[0], fontsize=15)
         ax.set_ylabel(labels[1], fontsize=15)
-        ax.grid()
-        return surf
+        ax.set_xlim([-3, 3])
+        ax.set_ylim([-3, 3])
+        if grid: ax.grid()
+        return cntr
