fix synthetic gradient DNI

dni/synthetic_grad_net.py
@@ -4,6 +4,9 @@ Synthetic Gradient network, summation of two binary digits
 import numpy as np
 import data
 import ann.lazy.nonlinearity as nonl
+import ann.lazy.cost_functions as cf
+import metrics.metrics as metr
+import utils.plotters as ut_plt
 
 
 class DNI(object):
@@ -42,50 +45,71 @@ class DNI(object):
         # ***
         # generate synthetic gradient: linear regression
         # ***
+        # self.syn_grad = self.output.dot(self.weights_dni) + self.bias_dni
+        # ***
+        # generate synthetic gradient: logistic regression
+        # ***
         self.syn_grad = self.nonlin(self.output.dot(self.weights_dni) + self.bias_dni)
-
         # update weights of the main network, using synthetic gradient
         delta_weights = self.syn_grad * self.nonlin_deriv(self.output)
-        # TODO test +
         self.weights -= self.alpha * self.input.T.dot(delta_weights)
         self.bias -= self.alpha * np.mean(delta_weights)
         return delta_weights.dot(self.weights.T), self.output
+
+    def forward(self, input):
+        """ Calculate forward network activated signal"""
+        self.input = input
+        self.output = self.nonlin(self.input.dot(self.weights) + self.bias)
+        return self.output
 
     def update_synthetic_weights(self, true_grad):
         """
         backprop for DNI network, true_grad comes from the above layer
         """
-        # TODO test +
+        alpha = self.alpha # 10 * self.alpha
+        # linear regression generator
+        # syn_grad_delta = (self.syn_grad - true_grad)
+        # logistic regression generator
         syn_grad_delta = (self.syn_grad - true_grad) * self.nonlin_deriv(self.syn_grad)
         self.weights_dni -= self.alpha * self.output.T.dot(syn_grad_delta)
         self.bias_dni -= self.alpha * np.mean(syn_grad_delta, axis=0)
 
 
+def accuracy(ref, pred):
+    """
+    Return micro F1 error on test data
+    :ref: reference, 2D array
+    :pred: prediction, 2D array
+    """
+    # p = np.argmax(pred, axis=1)
+    # p = cf.step(pred, threshold=0.5)
+    # return metr.micro_f1(y_true=ref, y_pred=p, accuracy=False)
+    return metr.pooled_accuracy(y_true=ref, y_pred=pred)
+
+
 if __name__ == '__main__':
     from utils import mnist_loader
 
-    # TODO add metric calculation
     data_type = 'mnist'
 
     if data_type == 'toy':
         num_examples = 1000
         output_dim = 12
         epoches = 1000
-        X, Y = data.generate_dataset(num_examples=num_examples, output_dim=output_dim)
+        X_train, Y_train = data.generate_dataset(num_examples=num_examples, output_dim=output_dim)
 
         batch_size = 256
         learn_rate = 0.1
-        nsamples, input_dim = X.shape
+        nsamples, input_dim = X_train.shape
         layer_1_dim = 64
         layer_2_dim = 32
-        nsamples, output_dim = Y.shape
+        nsamples, output_dim = Y_train.shape
     elif data_type == 'mnist':
         X_train, Y_train, X_val, Y_val, X_test, Y_test = mnist_loader.load_matrices()
         print("MNIST data is loaded...")
-        # MSE network with sigmoid output layer
-        epoches = 30
-        batch_size = 10
-        learn_rate = 3. # 3.0
+        epoches = 100
+        batch_size = 256
+        learn_rate = .1  # 3.0
         nsamples, input_dim = X_train.shape
         layer_1_dim = 64
         layer_2_dim = 32
@@ -96,26 +120,46 @@ if __name__ == '__main__':
     layer_2 = DNI(layer_1_dim, layer_2_dim, nonl.sigmoid, nonl.sigmoid_derivative, learn_rate)
     layer_3 = DNI(layer_2_dim, output_dim, nonl.sigmoid, nonl.sigmoid_derivative, learn_rate)
 
-    for iter in range(epoches+1):
-        error = 0
-        synthetic_error = 0
+    total_acc, total_cost_1, total_cost_2 = [], [], []
+
+    for iter in range(epoches + 1):
+        syn_error_2 = 0
+        syn_error_1 = 0
 
         for batch_i in range(int(nsamples / batch_size)):
             batch_x = X_train[(batch_i * batch_size):(batch_i + 1) * batch_size]
             batch_y = Y_train[(batch_i * batch_size):(batch_i + 1) * batch_size]
-
             # forward pass
             _, layer_1_out = layer_1.forward_and_synthetic_update(batch_x)
             delta_1, layer_2_out = layer_2.forward_and_synthetic_update(layer_1_out)
             delta_2, layer_3_out = layer_3.forward_and_synthetic_update(layer_2_out)
             # backward pass
+            # TODO use 2 SG generators
             delta_3 = layer_3_out - batch_y
             layer_3.update_synthetic_weights(delta_3)
             layer_2.update_synthetic_weights(delta_2)
             layer_1.update_synthetic_weights(delta_1)
 
-            # error += (np.sum(np.abs(delta_3 * layer_3_out * (1 - layer_3_out))))
-            error += np.sum(np.abs(delta_3 * nonl.sigmoid_derivative(layer_3_out)))
-            synthetic_error += (np.sum(np.abs(delta_2 - layer_2.syn_grad)))
+            # error += np.sum(np.abs(delta_3 * nonl.sigmoid_derivative(layer_3_out)))
+            syn_error_2 += np.linalg.norm(delta_3) / batch_size
+            syn_error_1 += np.linalg.norm(delta_2 - layer_2.syn_grad) / batch_size
         if (iter % 5 == 0):
-            print("\rIter:" + str(iter) + " Loss:" + str(error) + " Synthetic Loss:" + str(synthetic_error))
+            print("\rIter:" + str(iter) + " Synthetic_2:" + str(syn_error_2) + " Synthetic_1:" + str(syn_error_1))
+
+            # accuracy calculation on VAL dataset
+        layer_1_out = layer_1.forward(X_val)
+        layer_2_out = layer_2.forward(layer_1_out)
+        layer_3_out = layer_3.forward(layer_2_out)
+        acc = accuracy(ref=Y_val, pred=layer_3_out)
+        total_acc.append(acc)
+        total_cost_1.append(syn_error_1)
+        total_cost_2.append(syn_error_2)
+        print(total_acc[-1])
+    ut_plt.show_curves([total_acc],
+                       legend=['val error acc'],
+                       labels=["# of epochs", "value, %"],
+                       title='Error accuracy, sigmoid scores')
+    ut_plt.show_curves([total_cost_1, total_cost_2],
+                       legend=['syn_error_1', 'syn_error_2'],
+                       labels=["# of epochs", "value, %"],
+                       title='Error, sigmoid scores')

metrics/metrics.py
@@ -1,17 +1,42 @@
 import numpy as np
 
-def micro_f1(refs, predicts, accuracy=True):
+
+def micro_f1(y_true, y_pred, accuracy=True):
     """
-    :param refs, predicts: binary integer list of ndarrays,
+    :param y_true, y_pred: binary integer list of ndarrays,
         several hot-labels
     :return: accuracy or error of micro F1
     """
     # TODO example of usage
-    assert(len(refs) == len(predicts))
-    neg_r = np.logical_not(refs)
-    neg_p = np.logical_not(predicts)
-    tp = np.sum(np.logical_and(refs, predicts) == True)
-    fp = np.sum(np.logical_and(neg_r, predicts) == True)
-    fn = np.sum(np.logical_and(refs, neg_p) == True)
-    f1 = 100.0 * 2.0 * tp / (2.0*tp + fp + fn)
+    assert (len(y_true) == len(y_pred))
+    neg_r = np.logical_not(y_true)
+    neg_p = np.logical_not(y_pred)
+    tp = np.sum(np.logical_and(y_true, y_pred) == True)
+    fp = np.sum(np.logical_and(neg_r, y_pred) == True)
+    fn = np.sum(np.logical_and(y_true, neg_p) == True)
+    f1 = 100.0 * 2.0 * tp / (2.0 * tp + fp + fn)
     return accuracy and f1 or 100.0 - f1
+
+
+def step(a, threshold=0.0):
+    """Heaviside step function:
+    a < threshold = 0, else 1.
+    :param a: array
+    :return: array
+    """
+    res = np.zeros_like(a)
+    res[a < threshold] = 0
+    res[a >= threshold] = 1
+    return res
+
+
+def pooled_accuracy(y_true, y_pred):
+    """
+    y_true: 2D array
+    y_pred: 2D array
+    """
+    assert (len(y_true) == len(y_pred))
+    p = np.argmax(y_pred, axis=1)
+    y = np.argmax(y_true, axis=1)
+    N = float(len(y_true))
+    return np.sum(p == y) / N * 100.