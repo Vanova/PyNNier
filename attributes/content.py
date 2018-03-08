728de997df53ffd10eb8d0abf8d6e3bce5c85afa
Vanova <iva427@mail.ru>
2018-03-08 19:23:57 +0800

    implemented ark looping

--- attributes/content.py
+++ attributes/content.py
@@ -8,43 +8,55 @@ import utils.kaldi.io as kio
 
 MANNER_CLS = []
 PLACE_CLS = []
-ATTRIBUTES_CLS = {'manner': [''],
-              'place': ['']}
-ROOT_PATH = '/sipudata/pums/ivan/projects_data/mulan_lre17/train'
-n_jobs = 20
+ATTRIBUTES_CLS = {'manner': ['fricative', 'glides', 'nasal', 'other', 'silence', 'stop', 'voiced', 'vowel'],
+                  'place': ['coronal', 'dental', 'glottal', 'high', 'labial',
+                            'low', 'mid', 'other', 'palatal', 'silence', 'velar'],
+                  'fusion_manner': [],
+                  'fusion_place': []}
 
 
-def scan_folder(attrib_cls):
-    # loop lang dir
-    cluster_files = {}
-    lang_dirs = np.sort(os.listdir(ROOT_PATH))
-    for ldir in lang_dirs:
-        print('Language dir: %s' % ldir)
-        flist = []
-        for j in xrange(n_jobs):
-            p = '%s/res/%s/scores.%i.txt' % (ldir, attrib_cls, j + 1)
-            flist.append(p)
-        cluster_files[ldir] = flist
-    return cluster_files
+def scan_folder(lang_dir, attrib_cls):
+    print('Language dir: %s' % lang_dir)
+    flist = []
+    for j in xrange(n_jobs):
+        p = '%s/res/%s/scores.%i.txt' % (lang_dir, attrib_cls, j + 1)
+        flist.append(p)
+    return flist
 
 
-def scores_content():
-    # number of files
-    # hours
+# def scores_content():
+    # number of files per each language
+
+    # hours per each language
+
     # distributions of scores th = 0.5
+
     # score_file = path.join(ROOT_PATH, p)
     # score = kio.read_ark_file(score_file) # TODO make iterator
-    pass
+
 
 # define sampling: because some languages have more speech data
 # compare others
 
 if __name__ == '__main__':
-    # test lazy ark files reading
-    file_name = '../utils/kaldi/test.ark'
-    ark = kio.read_ark_file(file_name)
+    root_path = '/sipudata/pums/ivan/projects_data/mulan_lre17/train'
+    n_jobs = 20
+    debug = False
 
-    ark_iter = kio.ArkReader(file_name)
-    for id, arr in ark_iter.next_ark():
-        print('File: %s' % id)
-        print(arr.shape)
+    if debug:
+        file_name = '../utils/kaldi/test.ark'
+        ark_iter = kio.ArkReader(file_name)
+        for id, arr in ark_iter.next_ark():
+            print('File: %s' % id)
+            print(arr.shape)
+    else:
+        # loop through language clusters folder and calculate stats per language
+        lang_dirs = np.sort(os.listdir(root_path))
+        for ldir in lang_dirs:
+            flist = scan_folder(ldir, 'manner')
+
+            for f in flist:
+                ark_iter = kio.ArkReader(f)
+                for id, arr in ark_iter.next_ark():
+                    print('File: %s' % id)
+                    print(arr.shape)
